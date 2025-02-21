88e49f2b0511163c2acfdde68dc11291949fe43a
Danyel Granzotti <danyel.granzotti17@gmail.com>
2025-02-20 22:57:38 -0300

    feat: add bank account routes for retrieving and updating user bank account information

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.routes.user_routes import router as user_router
 from app.routes.health_routes import router as health_router
 from app.routes.supplier_routes import router as supplier_router
 from app.routes.purchase_routes import router as purchase_router
+from app.routes.bank_account_routes import router as bank_account_router
 
 app = FastAPI(
     title="FastAPI MongoDB API",
@@ -16,6 +17,7 @@ app.include_router(product_router, tags=["Products"])
 app.include_router(user_router, tags=["Users"])
 app.include_router(supplier_router, tags=["Suppliers"])
 app.include_router(purchase_router, tags=["Purchases"])
+app.include_router(bank_account_router, tags=["bank_accounts"])
 
 @app.get("/", tags=["Root"])
 async def read_root():
